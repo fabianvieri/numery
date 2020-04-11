5d51aa2ae3bc1dce63a9f23b6d7404af777c32bc
Fabian Vieri <vierifabian@gmail.com>
2020-04-11 10:55:33 +0700

    arrange initial folder structure

---
+++ src/containers/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+
+    <div class="alert alert-primary" role="alert">
+      A simple primary alert—check it out!
+    </div>
+  );
+}
+
+export default App;
