2f634c396a39cda867f4d351a97d37f86141e296
Fabian Vieri <vierifabian@gmail.com>
2020-04-13 20:00:22 +0700

    Updates

---
+++ precache-manifest.eab04f81ee454cd2f724edf848673cf0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6330b0091018ec2f7136ade534954c4",
+    "url": "/numery/index.html"
+  },
+  {
+    "revision": "11674fa4b45974e3a569",
+    "url": "/numery/static/css/2.07548e1c.chunk.css"
+  },
+  {
+    "revision": "21aaed16c42ad675cb21",
+    "url": "/numery/static/css/main.c28aaa4b.chunk.css"
+  },
+  {
+    "revision": "11674fa4b45974e3a569",
+    "url": "/numery/static/js/2.e08db1b8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/numery/static/js/2.e08db1b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21aaed16c42ad675cb21",
+    "url": "/numery/static/js/main.8137705a.chunk.js"
+  },
+  {
+    "revision": "d3dcb1d905fa4ae49d6e",
+    "url": "/numery/static/js/runtime-main.08f84654.js"
+  }
+]);
